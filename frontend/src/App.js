b05032fe0f7be0664e54f273adf2a233280f6ca3
Param <param23403@gmail.com>
2022-09-05 20:26:04 -0400

    UpdatedFrontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { HelloWorld } from "./Components/HelloWorld";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <HelloWorld />
-    </div>
+    <h1>hello World!</h1>
   );
 }
 
