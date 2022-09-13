9077eaba887d65deb4905a6c97c9a57b701de1d0
Michael Kodsi <tdq2kp@virginia.edu>
2022-09-13 01:05:01 -0400

    main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './Components/HelloWorld';
 
 function App() {
+  const pi = 3.14;
+  let x = "Maryland";
   return (
-    <h1>hello World!</h1>
+    <div>
+      <h1>UVA 2025</h1>
+      <HelloWorld display={x}/>
+      <HelloWorld display={pi}/>
+      <HelloWorld display={"2025"}/>
+      <HelloWorld display={"Go Hoos"}/>
+    </div>
   );
 }
 
---
+++ frontend/src/Components/HelloWorld.js
@@ -0,0 +1,8 @@
+const HelloWorld = (props) => {
+
+    return (
+        <h1>{props.display}</h1>
+    );
+};
+
+export default HelloWorld;
