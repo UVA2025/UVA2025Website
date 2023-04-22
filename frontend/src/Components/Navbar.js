3cc974373f556a43a47bd06210b50a97c096d11d
Michael Kodsi <tdq2kp@virginia.edu>
2023-04-22 18:38:50 -0400

    Fixed up navbar to be centered while logo on left

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -22,41 +22,44 @@ const theme = createTheme({
 
 const Navbar = () => {
     return (
-        <>
-            <Box sx={{ flexgrow: 1 }}>
-                <AppBar position="static">
-                    <ThemeProvider theme={theme}>
-                        <Toolbar style={{ backgroundColor: "#26365A" }} sx={{ justifyContent: "center" }} >
-                            <img src={require('../images/uva2025.png')} height={50} class='float-left' />
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <ThemeProvider theme={theme}>
+                    <Toolbar style={{ backgroundColor: "#26365A", display: 'flex', alignItems: 'center' }}>
+                        <img src={require('../images/uva2025.png')} height={50} alt="Logo" />
 
-                            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-                                <Button variant="text" color="secondary" onclick="">
-                                    HOME
-                                </Button>
-                            </Link>
+                        <Box sx={{ flexGrow: 1 }}></Box>
 
-                            <Link to="/events" style={{ textDecoration: "none", color: "white" }}>
-                                <Button variant="text" color="secondary" onclick="/events">
-                                    EVENTS
-                                </Button>
-                            </Link>
+                        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                            <Button variant="text" color="secondary" onClick="">
+                                HOME
+                            </Button>
+                        </Link>
 
-                            <Link to="/resources" style={{ textDecoration: "none", color: "white" }}>
-                                <Button variant="text" color="secondary" onclick="/resources">
-                                    RESOURCES
-                                </Button>
-                            </Link>
+                        <Link to="/events" style={{ textDecoration: "none", color: "white" }}>
+                            <Button variant="text" color="secondary" onClick="/events">
+                                EVENTS
+                            </Button>
+                        </Link>
 
-                            <Link to="/about" style={{ textDecoration: "none", color: "white" }}>
-                                <Button variant="text" color="secondary" onclick="/about">
-                                    ABOUT
-                                </Button>
-                            </Link>
-                        </Toolbar>
-                    </ThemeProvider>
-                </AppBar>
-            </Box>
-        </>
-    )
+                        <Link to="/resources" style={{ textDecoration: "none", color: "white" }}>
+                            <Button variant="text" color="secondary" onClick="/resources">
+                                RESOURCES
+                            </Button>
+                        </Link>
+
+                        <Link to="/about" style={{ textDecoration: "none", color: "white" }}>
+                            <Button variant="text" color="secondary" onClick="/about">
+                                ABOUT
+                            </Button>
+                        </Link>
+
+                        <Box sx={{ flexGrow: 1 }}></Box>
+                    </Toolbar>
+                </ThemeProvider>
+            </AppBar>
+        </Box>
+    );
 }
+
 export default Navbar;
