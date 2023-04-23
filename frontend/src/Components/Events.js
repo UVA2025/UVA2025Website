ff2a6891d0f10ef8b5e3904e25ef941dfda4dc4c
Michael Kodsi <tdq2kp@virginia.edu>
2023-04-23 11:31:14 -0400

    Initial commit, added months array

--- frontend/src/Components/Events.js
+++ frontend/src/Components/Events.js
@@ -1,7 +1,14 @@
-const Events =()=>{
+const Events = () => {
+
+    const months = Array.from({length: 12}, (item, i) => {
+        return new Date(0, i).toLocaleString('en-US', {month: 'long'})
+      });
+
+
     return (
+
         <>
-        This is the Events page
+            This is the Events page
         </>
     )
 
