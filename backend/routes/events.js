cec0521e1228b5e23d5225f46404b922474e2ffd
julian donald <jed5gpx@virginia.edu>
2023-04-22 21:46:24 -0400

    remove unnecessary comment

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const client = require('../services/contentfulClient').client;
 
 router.get('/', function(req, res, next) {
-  // TODO: filter data, just send "fields" to frontend
   // TODO: figure out how to distinguish between past/current events
   client.getEntries()
     .then(response => {const filteredItems = response.items.map(item => {
