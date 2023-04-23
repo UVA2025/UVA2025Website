6831c348b8fba1d6d1c2edc1a9df70782cb8699c
Michael Kodsi <tdq2kp@virginia.edu>
2023-04-23 11:34:55 -0400

    All months for next year (starting with current month) displayed

--- frontend/src/Components/Events.js
+++ frontend/src/Components/Events.js
@@ -1,15 +1,23 @@
 const Events = () => {
 
-    const months = Array.from({length: 12}, (item, i) => {
-        return new Date(0, i).toLocaleString('en-US', {month: 'long'})
-      });
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
 
+    const months = Array.from({ length: 12 }, (_, i) => {
+        const month = (currentMonth + i) % 12;
+        const year = currentYear + Math.floor((currentMonth + i) / 12);
+        return new Date(year, month).toLocaleString('en-US', { month: 'long' });
+    });
+
+    const listItems = months.map((name, index) => <li key={index}>{name}</li>);
 
     return (
 
-        <>
-            This is the Events page
-        </>
+        <div>
+            <h1>Months List:</h1>
+            <ul>{listItems}</ul>
+        </div>
     )
 
 }
