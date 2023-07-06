29da5fd1226325c6540681ea330dd86e8947be48
Michael Kodsi <tdq2kp@virginia.edu>
2023-07-06 00:10:31 -0400

    Usability fixes

--- frontend/src/Components/Events.js
+++ frontend/src/Components/Events.js
@@ -1,93 +1,111 @@
 import Typography from '@mui/material/Typography';
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Box } from '@mui/material';
 import Rotunda from '../images/Rotunda.jpeg';
 import EventCard from './EventCard';
 import React, { useEffect, useState } from 'react';
 
 const Events = () => {
-    const [events, setEvents] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const fetchEventData = () => {
-        fetch("http://localhost:9000/api/v1/events")
-            .then(response => response.json())
-            .then(data => {
-                setEvents(data);
-                setIsLoading(false);
-            });
-    };
+  const fetchEventData = () => {
+    fetch("http://localhost:9000/api/v1/events")
+      .then(response => response.json())
+      .then(data => {
+        setEvents(data);
+        setIsLoading(false);
+      });
+  };
 
-    useEffect(() => {
-        fetchEventData();
-    }, []);
+  useEffect(() => {
+    fetchEventData();
+  }, []);
 
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth();
-    const currentYear = currentDate.getFullYear();
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth();
+  const currentYear = currentDate.getFullYear();
 
-    const months = Array.from({ length: 12 }, (_, i) => {
-        const month = (currentMonth + i) % 12;
-        const year = currentYear + Math.floor((currentMonth + i) / 12);
-        return new Date(year, month)
-            .toLocaleString("en-US", { month: "long" })
-            .toLocaleUpperCase();
+  const months = Array.from({ length: 12 }, (_, i) => {
+    const month = (currentMonth + i) % 12;
+    const year = currentYear + Math.floor((currentMonth + i) / 12);
+    return new Date(year, month)
+      .toLocaleString("en-US", { month: "long" })
+      .toLocaleUpperCase();
+  });
+
+  const filteredMonths = months.filter((month) => {
+    const filteredEventItems = events.filter((event) => {
+      const isoDateString = event.dateAndTime;
+      const dateObject = new Date(isoDateString);
+
+      return dateObject
+        .toLocaleString("default", { month: "long" })
+        .toUpperCase()
+        .includes(month);
     });
 
-    return (
-        <div>
-            <div
-                style={{
-                    backgroundImage: `url(${Rotunda})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center center",
-                    backgroundRepeat: "no-repeat",
-                    width: "100vw",
-                    height: "60vh",
-                }}
-                className="front-page-image"
-            >
-                <div style={{ textAlign: "center", color: "white" }}>
-                    <Typography>
-                        <h1 style={{ margin: "0" }}>EVENTS</h1>
-                    </Typography>
-                </div>
-            </div>
-            <Container>
-                {isLoading ? (
-                    <Typography variant="h4" style={{ textAlign: "center" }}>
-                        Loading events...
-                    </Typography>
-                ) : (
-                    <>
-                        {months.map((month, index) => {
-                            const filteredEventItems = events.filter((event) => {
-                                const isoDateString = event.dateAndTime;
-                                const dateObject = new Date(isoDateString);
+    return filteredEventItems.length > 0;
+  });
 
-                                return dateObject
-                                    .toLocaleString("default", { month: "long" })
-                                    .toUpperCase()
-                                    .includes(month);
-                            });
-
-                            return (
-                                <div key={index}>
-                                    <Typography variant="h4">{month}</Typography>
-                                    <Grid container spacing={2}>
-                                        {filteredEventItems.map((event, eventIndex) => (
-                                            <Grid item xs={12} sm={6} key={eventIndex}>
-                                                <EventCard event={event} />
-                                            </Grid>
-                                        ))}
-                                    </Grid>
-                                </div>
-                            );
-                        })}
-                    </>
-                )}
-            </Container>
+  return (
+    <div>
+      <div
+        style={{
+          backgroundImage: `url(${Rotunda})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center center",
+          backgroundRepeat: "no-repeat",
+          width: "100vw",
+          height: "60vh",
+        }}
+        className="front-page-image"
+      >
+        <div style={{ textAlign: "center", color: "white" }}>
+          <Typography>
+            <h1 style={{ margin: "0" }}>EVENTS</h1>
+          </Typography>
         </div>
-    );
+      </div>
+      <Container>
+        {isLoading ? (
+          <Typography variant="h4" style={{ textAlign: "center" }}>
+            Loading events...
+          </Typography>
+        ) : (
+          <>
+            {filteredMonths.map((month, index) => {
+              const filteredEventItems = events.filter((event) => {
+                const isoDateString = event.dateAndTime;
+                const dateObject = new Date(isoDateString);
+
+                return dateObject
+                  .toLocaleString("default", { month: "long" })
+                  .toUpperCase()
+                  .includes(month);
+              });
+
+              return (
+                <div key={index}>
+                  <Box my={4}>
+                    <Typography variant="h4">{month}</Typography>
+                  </Box>
+                  <Grid container spacing={2}>
+                    {filteredEventItems.map((event, eventIndex) => (
+                      <Grid item xs={12} sm={6} key={eventIndex}>
+                        <EventCard event={event} />
+                      </Grid>
+                    ))}
+                  </Grid>
+                </div>
+              );
+            })}
+          </>
+        )}
+      </Container>
+      <br></br>
+      <br></br>
+    </div>
+  );
 };
 
 export default Events;
