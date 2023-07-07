7a2711a280f11eb9df006dd03185be24b7975708
Michael Kodsi <tdq2kp@virginia.edu>
2023-07-07 00:30:14 -0400

    Added some styling / date and description to card

--- frontend/src/Components/EventDetails.js
+++ frontend/src/Components/EventDetails.js
@@ -1,6 +1,61 @@
-import { Card, CardContent, Typography, Grid } from '@mui/material';
+import { Card, CardContent, Typography, Grid, CardMedia } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
+
+const formatDate = (dateString) => {
+    const options = {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+    };
+
+    const date = new Date(dateString);
+    const formattedDate = date.toLocaleDateString("en-US", options);
+
+    // Extract the day number from the date and add the appropriate suffix
+    const day = date.getDate();
+    const daySuffix = getDaySuffix(day);
+    const formattedDay = `${day}${daySuffix}`;
+
+    return formattedDate.replace(/(\d+\/\d+\/\d+)/, formattedDay);
+};
+
+const getDaySuffix = (day) => {
+    if (day >= 11 && day <= 13) {
+        return "th";
+    }
+
+    switch (day % 10) {
+        case 1:
+            return "st";
+        case 2:
+            return "nd";
+        case 3:
+            return "rd";
+        default:
+            return "th";
+    }
+};
+
+const getParagraphText = (event) => {
+    let text = "";
+
+    event.content.forEach((contentElement) => {
+        console.log("contentElement", contentElement);
+        if (contentElement.nodeType === 'paragraph') {
+            contentElement.content.forEach((textVal) => {
+                text += textVal.value;
+            })
+        }
+    });
+
+    console.log("text", text);
+
+    return text.trim();
+};
 
 
 const EventDetails = (props) => {
@@ -32,15 +87,32 @@ const EventDetails = (props) => {
   }
 
   return (
-    <div>
-      <Card>
-        <CardContent>
-          <Typography variant="h4">{event.eventName}</Typography>
-          <Typography variant="subtitle1">{event.dateAndTime}</Typography>
-          {/* <Typography variant="body1">{event.description}</Typography> */}
-        </CardContent>
-      </Card>
-    </div>
+    <Grid container spacing={2} justifyContent="center">
+      <Grid item xs={12} md={8} lg={6}>
+        <Card>
+          <div style={{ height: 300, overflow: 'hidden' }}>
+            <img
+              src={event.image.file.url}
+              alt={event.eventName}
+              style={{ width: '100%', objectFit: 'cover' }}
+            />
+          </div>
+          <CardContent>
+            <Typography variant="h5" gutterBottom>
+              {event.eventName}
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+              {formatDate(event.dateAndTime)}
+            </Typography>
+            <Typography variant="body1" paragraph>
+              {console.log("event NEW", event)}
+            </Typography>
+            {/* Additional event details or components */}
+            {getParagraphText(event)}
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
