b561cec6eea3fd6d9318d62ad453fd953d6ca8a6
Michael Kodsi <tdq2kp@virginia.edu>
2023-08-21 15:25:38 -0400

    frontend only

--- backend/services/contentfulClient.js
+++
@@ -1,9 +0,0 @@
-const contentful = require('contentful');
-require('dotenv').config();
-
-const client = contentful.createClient({
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-  space: process.env.CONTENTFUL_SPACE
-});
-
-exports.client = client;
